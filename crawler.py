cfe813d6ed8d083bf295ac2fb3b1e6161bbeb8b9
elenacalcan26 <elena.calcan26@gmail.com>
2023-12-21 22:07:36 +0200

    download paaps file from 3 ministers' websites

--- crawler.py
+++ crawler.py
@@ -1,5 +1,9 @@
 import requests
 import logging
+import os
+import re
+
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
@@ -8,12 +12,15 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# Ministerul Mediului
-base_url = "http://www.mmediu.ro/categorie/programul-anual-al-achizitiilor-publice/167"
+PAPs_PATTERN = r'(paap|\s*achizitii\s*publice)'
+
+base_url_1 = "https://www.mcid.gov.ro/interes-public/achizitii-publice/"
+base_url_2 = "https://ms.ro/informatii-de-interes-public/achizitii-publice/"
+base_url_3 = "http://www.mmediu.ro/categorie/programul-anual-al-achizitiilor-publice/167"
 
 visited = []
 
-urls_to_visit = [base_url]
+urls_to_visit = [base_url_1, base_url_2, base_url_3]
 
 
 def get_linked_url(url, html):
@@ -32,15 +39,41 @@ def crawl(url):
     html = requests.get(url).text
 
     for url in get_linked_url(url, html):
-        # if url not in visited and url not in urls_to_visit and (".pdf" in url or ".xls" in url):
-        #     urls_to_visit.append(url)
-        if url not in visited and url not in urls_to_visit:
-            urls_to_visit.append(url)
+        if ".pdf" in url or ".xls" in url:
+            download_file(url)
+
+
+def download_file(url):
+    response = requests.get(url)
+    filename = url.rpartition('/')[-1]
+    logging.info(f'Found file: {filename}')
+
+    if (is_pap_file(filename)):
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+            f.close
+            logging.info('Download completed!')
+
+
+def is_pap_file(filename):
+    pattern = re.compile(PAPs_PATTERN, re.IGNORECASE)
+    return bool(pattern.search(filename))
+
+def setup_dir(url):
+    base_dir = os.getcwd()
+    dir_name = urlparse(url).netloc
+
+    logging.info(f'Creating directory {dir_name}')
+    os.mkdir(base_dir + '/' + dir_name)
+    os.chdir(f'./{dir_name}')
 
 
 def run():
     while urls_to_visit:
         url = urls_to_visit.pop(0)
+
+        setup_dir(url)
+
         logging.info(f'Crawling: {url}')
 
         try:
@@ -50,6 +83,7 @@ def run():
         finally:
             visited.append(url)
 
+        os.chdir('../')
 
 if __name__ == '__main__':
     run()
